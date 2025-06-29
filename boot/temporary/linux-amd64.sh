39ab943ee33952f942962aabdd634175a9ad2627
LukeGrahamLandry <graham11000@gmail.com>
2025-06-29 07:21:54 -0400

    whatever

--- boot/temporary/linux-amd64.sh
+++ boot/temporary/linux-amd64.sh
@@ -1,4 +1,4 @@
 mkdir target
-curl https://lukegrahamlandry.ca/franca/bin/fb35471e34c5b707341542f1a8176219e80fbc23/franca-linux-amd64-dyn -o ./target/boot-linux-amd64-dyn.out
+curl https://lukegrahamlandry.ca/franca/bin/bd44f7952dd8a61ad73a3e6796068ec60c890409/franca-linux-amd64-dyn -o ./target/boot-linux-amd64-dyn.out
 chmod +x ./target/boot-linux-amd64-dyn.out
 ./target/boot-linux-amd64-dyn.out run_tests.fr -- core
--- boot/temporary/macos-amd64.sh
+++ boot/temporary/macos-amd64.sh
@@ -1,4 +1,4 @@
 mkdir target
-curl https://lukegrahamlandry.ca/franca/bin/fb35471e34c5b707341542f1a8176219e80fbc23/franca-macos-amd64 -o ./target/boot-macos-amd64.out
+curl https://lukegrahamlandry.ca/franca/bin/bd44f7952dd8a61ad73a3e6796068ec60c890409/franca-macos-amd64 -o ./target/boot-macos-amd64.out
 chmod +x ./target/boot-macos-amd64.out
 ./target/boot-macos-amd64.out run_tests.fr -- core
